f30d8b6e7d33c69209e93f23afc066ea289ee5ce
ysdede <yunusseyhandede@gmail.com>
2022-11-18 20:31:05 +0300

    Fixes.

--- binanceBookTicker.py
+++ binanceBookTicker.py
@@ -1,54 +1,60 @@
-# -*- coding: utf8 -*-
-from __future__ import print_function
-from __future__ import division
 from pymongo import MongoClient
 from datetime import datetime
 import pytz, time
 from colorama import init
 from colorama import Fore, Style
 import requests
-
+from vars import ticker_object, mappings, quote_assets
 init()
 
-interval = 30
-
+interval = 15
+# quoteAssets = ["TRY", "USDT"]
+exchange = 'binance'
 client = MongoClient()
 db = client.binance_bookTicker
 bookTickerURL = """https://api.binance.com/api/v3/ticker/bookTicker"""
 
 d = datetime.now(tz=pytz.utc)
-dFormatted = Fore.GREEN + d.strftime('%d/%m/%Y %H:%M:%S') + Style.RESET_ALL
-print("{} Binance bookTicker started...".format(dFormatted))
+dFormatted = Fore.GREEN + d.strftime("%d/%m/%Y %H:%M:%S") + Style.RESET_ALL
+print(f"{dFormatted} Binance bookTicker started...")
 
 while True:
     d = datetime.now(tz=pytz.utc)
     ts = time.time()
-    if(not d.second % interval):
-        print ("{} Interval".format(d.strftime('%d/%m/%Y %H:%M:%S')))
+    if not d.second % interval:
+        start = time.time()
+        record = ticker_object.copy()
+        symbol_done = 0
+        print(f'{d.strftime("%d/%m/%Y %H:%M:%S")} Interval')
         try:
             response = requests.get(bookTickerURL)
-            print(response)
-            sayfa = response
-            print(sayfa)
             data_json = response.json()
-        except:
+        except Exception as e:
             status = 0
-            print('Binance api hatası')
+            print(f"Binance api hatası.\n {e}")
 
         for i in range(len(data_json)):
-            symbol = data_json[i]['symbol']
-            bidPrice = data_json[i]['bidPrice']
-            bidQty = data_json[i]['bidQty']
-            askPrice = data_json[i]['askPrice']
-            askQty = data_json[i]['askQty']
-            data_json[i]['timestamp'] = d
-            data_json[i]['_id'] = int(ts)
-            result = db[symbol].insert_one(data_json[i])
-            print(result)
-            dFormatted = Fore.GREEN + d.strftime('%d/%m/%Y %H:%M:%S') + Style.RESET_ALL
-            print("{} Inserted_id: {}, {}, {}, {}".format(dFormatted, result.inserted_id, symbol, askPrice, bidPrice))
+            symbol = data_json[i]["symbol"]
+            
+            if not symbol.endswith(tuple(quote_assets[exchange])):
+                continue
 
-        print('Binance bookTicker done...')
+            bidPrice = float(data_json[i]["bidPrice"])
+            bidQty = float(data_json[i]["bidQty"])
+            askPrice = float(data_json[i]["askPrice"])
+            askQty = float(data_json[i]["askQty"])
+            data_json[i]["timestamp"] = d
+            data_json[i]["_id"] = int(ts)
+            try:
+                result = db[symbol].insert_one(data_json[i])
+                dFormatted = (
+                Fore.GREEN + d.strftime("%d/%m/%Y %H:%M:%S") + Style.RESET_ALL)
+                print(f"{dFormatted} Inserted_id: {result.inserted_id}, {symbol}, {askPrice}, {bidPrice}")
+                symbol_done += 1
+            except Exception as e:
+                print(f"MongoDB hatası.\n {e}")
+
+        print(f"Binance bookTicker done. Processed {symbol_done} symbols in {time.time() - start:0.1f} seconds...")
         client.close()
         time.sleep(0.9)
     time.sleep(0.9)
